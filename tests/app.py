56dde8c1ef00ffc286cfffc3c192efa33a460aaf
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-02-22 16:01:05 +0000

    WIP unit test app

---
+++ tests/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from govuk_frontend_wtf.main import WTFormsHelpers
+from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "M15zC@#&nMMj@J91IONM3CVubJDVIh$H"
+
+app.jinja_loader = ChoiceLoader(
+    [
+        PackageLoader("app"),
+        PrefixLoader(
+            {
+                "govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja"),
+                "govuk_frontend_wtf": PackageLoader("govuk_frontend_wtf"),
+            }
+        ),
+    ]
+)
+
+WTFormsHelpers(app)
---
+++ tests/test.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+python3 -m test_wtf_widgets.py

a393d2c2e837239a2e61217a2b5e4c79dccf1a51
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-05-05 14:27:09 +0100

    exclude tests from package

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LandRegistry/govuk-frontend-wtf",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude="tests"),
     package_data={"govuk_frontend_wtf": templates},
     classifiers=[
         "Development Status :: 4 - Beta",

65ef80546a4f951a3b375f6a65c7ddc9fa47b4b8
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-05-28 16:50:09 +0100

    black formatting

--- govuk_frontend_wtf/wtforms_widgets.py
+++ govuk_frontend_wtf/wtforms_widgets.py
@@ -161,36 +161,44 @@ class GovDateInput(GovFormBase):
     def map_gov_params(self, field, **kwargs):
         params = super().map_gov_params(field, **kwargs)
         if field._value():
-            day, month, year = field._value().split(' ')
-        params.setdefault('fieldset', {
-            'legend': {
-                'text': field.label.text
-            },
-        })
-        params.setdefault('items', [
-            {
-                'label': 'Day',
-                'id': '{}-day'.format(field.name),
-                'name': field.name,
-                'classes': ' '.join([
-                    'govuk-input--width-2',
-                    'govuk-input--error' if field.errors else ''
-                ]).strip(),
-                'value': day,
-            },
-            {
-                'label': 'Month',
-                'id': '{}-month'.format(field.name),
-                'name': field.name,
-                'classes': ' '.join([
-                    'govuk-input--width-2',
-                    'govuk-input--error' if field.errors else ''
-                ]).strip(),
-                'value': month,
-            },
+            day, month, year = field._value().split(" ")
+        params.setdefault(
+            "fieldset",
             {
                 "legend": {"text": field.label.text},
             },
+        )
+        params.setdefault(
+            "items",
+            [
+                {
+                    "label": "Day",
+                    "id": "{}-day".format(field.name),
+                    "name": field.name,
+                    "classes": " ".join(
+                        [
+                            "govuk-input--width-2",
+                            "govuk-input--error" if field.errors else "",
+                        ]
+                    ).strip(),
+                    "value": day,
+                },
+                {
+                    "label": "Month",
+                    "id": "{}-month".format(field.name),
+                    "name": field.name,
+                    "classes": " ".join(
+                        [
+                            "govuk-input--width-2",
+                            "govuk-input--error" if field.errors else "",
+                        ]
+                    ).strip(),
+                    "value": month,
+                },
+                {
+                    "legend": {"text": field.label.text},
+                },
+            ],
         )
         params.setdefault(
             "items",

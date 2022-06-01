f3109824a77efccb167723c9b17cb3490d2f10ad
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-06-01 14:24:30 +0100

    regenerate secret key

--- tests/app.py
+++ tests/app.py
@@ -4,7 +4,7 @@ from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 from govuk_frontend_wtf.main import WTFormsHelpers
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "M15zC@#&nMMj@J91IONM3CVubJDVIh$H"  # nosec
+app.config["SECRET_KEY"] = "405eb39c8ab0d1ab4a4ff56657a0d7aebf8ed079d4c5466a4933c72703a135f6"  # nosec
 
 app.jinja_loader = ChoiceLoader(
     [

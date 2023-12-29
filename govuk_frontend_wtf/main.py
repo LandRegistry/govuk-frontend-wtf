20e22e7ef2871af022013ca780c8c239f3eeee1c
Andrii Iudin <Andrii.Iudin@ukhsa.gov.uk>
2023-12-29 14:51:04 +0000

    Use ids where possible instead of field names as user might have provided ids. Othewise, the rendered clickable error would not shift the screen to the correct HTML anchor.

--- govuk_frontend_wtf/main.py
+++ govuk_frontend_wtf/main.py
@@ -20,21 +20,29 @@ class WTFormsHelpers(object):
 def wtforms_errors(form, params={}):
     wtforms_params = {"titleText": "There is a problem", "errorList": []}
 
-    wtforms_params["errorList"] = flatten_errors(form.errors)
+    id_map = {}
+    for field_name in form._fields.keys():
+        field = getattr(form, field_name, None)
+        if field and hasattr(field, 'id'):
+            id_map[field_name] = field.id
+
+    wtforms_params["errorList"] = flatten_errors(form.errors, id_map=id_map)
 
     return merger.merge(wtforms_params, params)
 
 
-def flatten_errors(errors, prefix=""):
+def flatten_errors(errors, prefix="", id_map={}):
     """Return list of errors from form errors."""
     error_list = []
     if isinstance(errors, dict):
         for key, value in errors.items():
             # Recurse to handle subforms.
-            error_list += flatten_errors(value, prefix=f"{prefix}{key}-")
+            if key in id_map:
+                key = id_map[key]
+            error_list += flatten_errors(value, prefix=f"{prefix}{key}-", id_map=id_map)
     elif isinstance(errors, list) and isinstance(errors[0], dict):
         for idx, error in enumerate(errors):
-            error_list += flatten_errors(error, prefix=f"{prefix}{idx}-")
+            error_list += flatten_errors(error, prefix=f"{prefix}{idx}-", id_map=id_map)
     elif isinstance(errors, list):
         error_list.append({"text": errors[0], "href": "#{}".format(prefix.rstrip("-"))})
     else:
